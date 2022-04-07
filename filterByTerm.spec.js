74583225747aac69b72fb25257e41b7081619565
kevinjacob3489 <94436183+kevinjacob3489@users.noreply.github.com>
2022-04-07 19:27:00 -0400

    Files

---
+++ filterByTerm.spec.js
@@ -0,0 +1,42 @@
+const filterByTerm = require("../src/filterByTerm");
+
+describe("Filter function", () => {
+    test("it should filter by a search term (link)", () => {
+        const input = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+        ];
+
+        const output = [{ id: 3, url: "https://www.link3.dev" }];
+
+        const output1 = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" }
+        ]
+
+        const alloutputs = [
+            { id: 1, url: "https://www.url1.dev" },
+            { id: 2, url: "https://www.url2.dev" },
+            { id: 3, url: "https://www.link3.dev" }
+        ]
+
+        expect(filterByTerm(input, "link")).toEqual(output);
+
+        expect(filterByTerm(input, "LINK")).toEqual(output);
+
+        //Exercise
+        //a)
+        expect(filterByTerm(input, "uRl")).toEqual(output1);
+
+        //b)
+        expect(filterByTerm(input, "")).toEqual(alloutputs);
+    });
+});
+
+function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i");
+    return inputArr.filter(function (arrayElement) {
+        return arrayElement.url.match(regex);
+    });
+}
